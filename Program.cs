7f5bab815aebaf142f6cabce263b729edfcc44a2
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-05-04 17:55:27 -0400

    Added deserialization

--- Program.cs
+++ Program.cs
@@ -7,15 +7,35 @@ using System.Threading.Tasks;
 
 namespace ApiClient
 {
+    class Joke
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("setup")]
+        public string Setup { get; set; }
+        [JsonPropertyName("punchline")]
+        public string Punchline { get; set; }
+    }
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
-            var responseAsString = await client.GetStringAsync("https://official-joke-api.appspot.com/jokes/random");
+            var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_joke");
 
 
-            Console.WriteLine(responseAsString);
+            var jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+            foreach (var item in jokes)
+            {
+                Console.WriteLine($"The ID of the joke is {item.Id} and it is a {item.Type} type joke. The setup: {item.Setup}, the punchline: {item.Punchline}.");
+            }
+            //Menu
+            Console.WriteLine();
+
+
+
         }
     }
 }
