36e83488d7c0cf16b9ed2e07c8aa28fcdb4fbb8e
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-05-04 20:48:18 -0400

    Added programming jokes

--- Program.cs
+++ Program.cs
@@ -36,40 +36,54 @@ namespace ApiClient
             {
                 Console.WriteLine("Would you like some (R)andom jokes or (P)rogramming jokes?   ");
                 var jokeType = Console.ReadLine().ToUpper();
-                Console.WriteLine("Do you want to hear one joke, or ten?   ");
+                Console.WriteLine("Do you want to hear one joke, or ten? Yeah, I know it's weird, but that's all the API allows for. (One or Ten)   ");
                 var jokeQuantity = Console.ReadLine().ToUpper();
                 //Tell 1 random joke
                 if (jokeType == "R" && jokeQuantity == "ONE")
                 {
-                    var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_joke");
-                    var jokes = await JsonSerializer.DeserializeAsync<Joke>(responseAsStream);
+                    var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/random");
+                    var joke = await JsonSerializer.DeserializeAsync<Joke>(responseAsStream);
                     Console.WriteLine();
-                    Console.WriteLine(jokes.Setup);
-                    Console.WriteLine(jokes.Punchline);
+                    Console.WriteLine(joke.Setup);
+                    Console.WriteLine(joke.Punchline);
+                    Console.WriteLine();
                 }
                 //Tell 10 random jokes
                 if (jokeType == "R" && jokeQuantity == "TEN")
                 {
-
+                    var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/ten");
+                    var jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+                    Console.WriteLine();
+                    foreach (var joke in jokes)
+                    {
+                        Console.WriteLine(joke.Setup);
+                        Console.WriteLine(joke.Punchline);
+                        Console.WriteLine();
+                    }
                 }
                 //Tell 1 programming joke
                 if (jokeType == "P" && jokeQuantity == "ONE")
                 {
                     var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/programming/random");
-                    var jokes = await JsonSerializer.DeserializeAsync<Joke>(responseAsStream);
+                    var joke = await JsonSerializer.DeserializeAsync<Joke>(responseAsStream);
                     Console.WriteLine();
-                    Console.WriteLine(jokes.Setup);
-                    Console.WriteLine(jokes.Punchline);
+                    Console.WriteLine(joke.Setup);
+                    Console.WriteLine(joke.Punchline);
+                    Console.WriteLine();
                 }
                 //Tell 10 programming jokes
                 if (jokeType == "P" && jokeQuantity == "TEN")
                 {
-
+                    var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/programming/ten");
+                    var jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+                    Console.WriteLine();
+                    foreach (var joke in jokes)
+                    {
+                        Console.WriteLine(joke.Setup);
+                        Console.WriteLine(joke.Punchline);
+                        Console.WriteLine();
+                    }
                 }
-
-
-
-
 
                 Console.WriteLine();
                 Console.WriteLine("Do you want some more jokes? (Yes or No)   ");
